799fc34e6ef723117d02b87fe7589afca034e72d
Rajeev Singh <rajeevsinghece49@gmail.com>
2023-12-06 23:57:24 +0530

    Put, Delete API added

--- src/main/java/com/codewithrajeev/blog/controllers/UserController.java
+++ src/main/java/com/codewithrajeev/blog/controllers/UserController.java
@@ -3,12 +3,16 @@ package com.codewithrajeev.blog.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codewithrajeev.blog.payloads.ApiResponse;
 import com.codewithrajeev.blog.payloads.UserDto;
 import com.codewithrajeev.blog.services.UserService;
 
@@ -26,6 +30,21 @@ public class UserController {
 		return new ResponseEntity<>(createdUserDto, HttpStatus.CREATED);
 		
 	}
+	
+	@PutMapping("/{userId}")
+	public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable Integer userId ){
+		UserDto updatedUser = this.userService.updateUser(userDto, userId);
+		return ResponseEntity.ok(updatedUser);
+	}
+	
+	
+	@DeleteMapping("/{userId}")
+	public ResponseEntity<ApiResponse> deleteUser (@PathVariable Integer userId){
+		 this.userService.deleteUser(userId);
+		 return new ResponseEntity<>(new ApiResponse("User Deleted Successfully", "true"),HttpStatus.OK);
+	}
+	
+	
 	
 
 }
---
+++ src/main/java/com/codewithrajeev/blog/payloads/ApiResponse.java
@@ -0,0 +1,20 @@
+package com.codewithrajeev.blog.payloads;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse {
+	
+	private String message;
+	private String status;
+
+	 
+	 
+}
