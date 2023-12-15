9e69fd5b0886abf98e1f7c5991c4a94b8541ecd0
Rajeev Singh <rajeevsinghece49@gmail.com>
2023-12-15 15:29:18 +0530

    Post Entity and Relationship

--- src/main/java/com/codewithrajeev/blog/entities/Category.java
+++ src/main/java/com/codewithrajeev/blog/entities/Category.java
@@ -1,10 +1,16 @@
 package com.codewithrajeev.blog.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,6 +29,10 @@ public class Category {
 	
 	@Column(name="title", length=100,nullable =false)
 	private String categoryTitle;
+	
 	@Column(name="description",length=300,nullable =false)
 	private String categoryDescription;
+	
+	@OneToMany(mappedBy = "category", cascade= CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Post> posts = new ArrayList<>();
 }
